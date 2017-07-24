c283405f7268de44b0ed3bcc640ed61ff7d93d0d
nostronia <nostronia@yahoo.com>
2017-07-24 11:56:29 -0600

    Added misc comment to ViewController.swift

--- GIT_Throwaway/ViewController.swift
+++ GIT_Throwaway/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        // I am adding this to do nothing.  Just a test.
+        
     }
 
 
